9c47f626ac1e1cc224790ee5edd2c8a7d33d804b
0x100 <0x100.me@gmail.com>
2018-04-30 11:06:43 +0200

    Initial Commit

---
+++ BLEScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEScanner
+//
+//  Created by Simon on 30.04.18.
+//  Copyright © 2018 Simon J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
